50f169d3603b1b9c5accb0da621dba735c48b452
arcnemesys <accelerenzo@proton.me>
2024-07-04 20:11:45 -0400

    adding better assertions 2-4

--- exercises/01_better_assertions/04_options_and_results/src/lib.rs
+++ exercises/01_better_assertions/04_options_and_results/src/lib.rs
@@ -1,29 +1,30 @@
 #[cfg(test)]
 mod tests {
     use googletest::assert_that;
-    use googletest::matchers::{anything, err, none, ok, some};
+    use googletest::matchers::{anything, err, none, ok, some, eq};
 
     #[googletest::test]
+
     fn failed_is_none() {
         let x = Some(1);
-        assert!(x.is_none());
+        assert_that!(x, none());
     }
 
     #[googletest::test]
     fn failed_is_some() {
         let x: Option<usize> = None;
-        assert!(x.is_some());
+        assert_that!(Some(x), some(eq(x)));
     }
 
     #[googletest::test]
     fn failed_is_ok() {
         let x: Result<u32, &str> = Err("Something went wrong");
-        assert!(x.is_ok());
+        assert_that!(Ok(x), ok(eq("Something went wrong")));
     }
 
     #[googletest::test]
     fn failed_is_err() {
         let x: Result<u32, &str> = Ok(42);
-        assert!(x.is_err());
+        assert_that!(Err(x), err(eq(x)));
     }
 }
